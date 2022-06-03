42feb6ff49be70712f929f983c19257adf14ab53
eles linux <eleswastaken@gmail.com>
2022-06-03 07:12:32 +0600

    Create widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,31 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: TimerHomePage(),
-    primarySwatch: Color.black,
     ));
 }
-
 
 class TimerHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+	String _aString = 'hello h string';
+
     return Scaffold(
+		backgroundColor: Colors.grey,
         body: Center(
-            child: Text('hello app'),
+            child: Column(
+			mainAxisAlignment: MainAxisAlignment.center,
+				children: [
+					Text('Focus'),
+					Text('00:00'),
+					Text('Session Number'),
+					Row(
+						mainAxisAlignment: MainAxisAlignment.center,
+						children: [
+							ElevatedButton( onPressed: () {}, child: Text('Start')),
+							ElevatedButton( onPressed: () {}, child: Text('Pause')),
+					]),
+			],),
           ),
       );
   }
